0c96b76255f5f642c849a92089cadcd13ed29f3a
Andrei Lisnic <andrei.lisnic@gmail.com>
2012-12-11 20:48:01 +0200

    remove factories from previous project

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,32 +1,3 @@
 FactoryGirl.define do
-  factory :terminal do
-    name { SecureRandom.hex(10) }
-    uri { SecureRandom.hex(8) }
-  end
 
-  factory :connection do
-    association :terminal
-    port { rand(64000) }
-    user { SecureRandom.hex(10) }
-    protocol { Connection::PROTOCOLS.sample }
-    credentials { SecureRandom.hex(10) }
-  end
-
-  factory :remote_session do
-    association :connection
-  end
-
-  factory :head do
-    sequence(:email) {|n| "head#{n}@test.com " }
-    password "qweqwe"
-    password_confirmation "qweqwe"
-    association :organization
-  end
-
-  factory :user do
-    sequence(:email) {|n| "user#{n}@test.com " }
-    password "qweqwe"
-    password_confirmation "qweqwe"
-    association :organization
-  end
 end
